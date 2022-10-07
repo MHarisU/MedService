3b3c28a6592941201e6193dd46293816930f2e4f
burhanzoaib99 <112868615+burhanzoaib99@users.noreply.github.com>
2022-10-07 17:29:11 +0500

    - 7 Oct 2022 5Pm   * Added Doctor Registration Activity   * Completed Patient Registration Activity

---
+++ app/src/main/java/com/med/medservice/NewUI/RegisterDoctorNewActivity.java
@@ -0,0 +1,20 @@
+package com.med.medservice.NewUI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.med.medservice.R;
+
+public class RegisterDoctorNewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_doctor_new);
+    }
+
+    public void Close(View view) {
+    }
+}
